9cfd6d62c906d85cbfa083b23a1f8717c08ee027
Matthew King <matt@valiantsoftware.com.au>
2024-05-09 10:44:18 +0800

    Add publish script for Windows.

---
+++ x-publish-win-x64.ps1
@@ -0,0 +1,1 @@
+dotnet publish "$PSScriptRoot/src/ValiantTemplate.Photino/ValiantTemplate.Photino.csproj" --output "$PSScriptRoot/artifacts" --configuration Release --runtime win-x64 --self-contained
